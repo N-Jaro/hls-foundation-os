d49c42f5f2c0148764532a54dce9b3c998fff1fe
Nattapon Jaroenchai <b_nj18@live.com>
2024-03-04 12:13:58 -0600

    Update test_loader.py

--- configs/test_loader.py
+++ configs/test_loader.py
@@ -7,10 +7,30 @@ dist_params = dict(backend="nccl")
 log_level = "INFO"
 
 dataset_type = "MapSegDataset"
+img_suffix = ".png"
+seg_map_suffix = "_mask.png"
 
 # TO BE DEFINED BY USER: data directory
 data_root = "/projects/bbym/nathanj/hls-foundation-os/data/map_seg"
+
 train_pipeline = [
     dict(type="LoadMapSegDataPatch"),
     dict(type="LoadMapSegAnnotations"),
 ]
+
+CLASSES = ("non seg", "seg")
+
+data = dict(
+    train=dict(
+        type=dataset_type,
+        CLASSES=CLASSES,
+        data_root=data_root,
+        img_dir="training",
+        ann_dir="training",
+        img_suffix=img_suffix,
+        seg_map_suffix=seg_map_suffix,
+        pipeline=train_pipeline,
+        ignore_index=-1,
+    ),
+)
+
