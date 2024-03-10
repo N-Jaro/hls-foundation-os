8f580b77b7729ac810b15f10db105bfd3b03317c
Nattapon Jaroenchai <b_nj18@live.com>
2024-03-10 18:24:51 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -7,11 +7,13 @@
 #SBATCH --gpus-per-node=2
 #SBATCH --output=/projects/bbym/nathanj/hls-foundation-os/map_seg_1.out
 
+eval "$(conda shell.bash hook)"
+
 #activate conda
 conda activate geo_foundation
 
-# Load required modules
-module load anaconda3_gpu cudnn
+# # Load required modules
+# module load anaconda3_gpu cudnn
 
 # Change directory
 cd /projects/bbym/nathanj/hls-foundation-os
